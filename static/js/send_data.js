7b3c2081858de0d5f6f61f584e8a0abaa7d29027
felipepmartinez <felipepmartinez@gmail.com>
2016-04-30 00:54:26 -0300

    Add JQuery UI Datepicker
    Datepicker on date fields. Some more validations added to the form.

--- static/js/send_data.js
+++ static/js/send_data.js
@@ -3,21 +3,53 @@ $( document ).ready(function() {
     // se o checkbox ta apertado dataentrada e datasaida recebem null
     // 		+ desativa as caixas
 
+ 	$(".inputData").datepicker({
+ 		dateFormat: 'yy-mm-dd',
+ 		defaultDate: '-1y'
+ 	});
+
+ 	/** nao funciona de jeito nenhum
+
+ 	$("#inputInicio").datepicker({
+ 		onClose: function(selectedDate) {
+        	alert("entrou");
+        	$("#inputFim").datepicker("option", "minDate", selectedDate);
+      	}
+    });
+
+ 	$("#inputFim").datepicker({
+ 		onClose: function(selectedDate) {
+        	$("#inputInicio").datepicker("option", "maxDate", selectedDate);
+      	}
+    });
+	
+	**/
+
     $('#inputCheckbox').click(function() {
 
-    	$('#inputInicio').attr('disabled', this.checked);
-    	$('#inputFim').attr('disabled', this.checked);
+    	$('.inputData').attr('disabled', this.checked);
 
     	if (this.checked) {
-    		$('#inputInicio').val(null);
-    		$('#inputFim').val('');
+    		$('#inputData').val('');
     	}
 
 	});
 
  	$("#buttonBuscar").click(function() {
 
-    	var data = JSON.stringify({"local":$('#inputLocal').val(),"inicio":$('#inputInicio').val(),"fim":$("#inputFim").val()});
+    	var data = JSON.stringify({"local":$('#inputLocal').val(),
+    						"inicio":$('#inputInicio').val(),
+    						"fim":$("#inputFim").val() });
+
+    	if ($('#inputInicio').val() > $('#inputFim').val()) {
+    		alert("A data de entrada deve ser menor que a de saída!");
+    		return;
+    	}
+
+    	if ($("#inputLocal").val() == '') {
+    		alert("É preciso entrar com um valor para o local!");
+    		return;
+    	}
 
     	$('#listaHoteis').empty();
 
