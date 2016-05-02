1588ec3991b67153269a01dcd36c55e62bd2c011
felipepmartinez <felipepmartinez@gmail.com>
2016-05-02 05:18:08 -0300

    Remove /static from repository
    This folder was renamed to public

--- api/routes.js
+++ api/routes.js
@@ -7,7 +7,7 @@ var initDatabase = require('../config/database_setup');
 
 module.exports = function (app, database) {
 
-// =============== import route ===========================
+// ================== import route ===========================
 
 	app.get('/import', function (req, res) {
 
@@ -83,5 +83,116 @@ module.exports = function (app, database) {
 		});
 	});
 
+// ================== select route ===========================
+
+	app.post('/select', function (req, res) {
+		
+		var local = req.body["local"];
+		var dataInicio = req.body["inicio"];
+		var dataFim = req.body["fim"];
+
+		var cacheKey = local + dataInicio + dataFim;
+
+		// check if the key is cached
+		cache.get(cacheKey, function (err, result) {
+			if (err) throw err;
+
+			if (result == undefined) {
+				// not in cache: manual search
+				database.getConnection(function (err, connection) {
+					if (err) throw err;
+					connection.query('USE hotelurbano', function (err) {
+						if (err) throw err;
+
+						sql =   "SELECT * " 
+								+ "FROM	hoteis "
+								+ "WHERE (nome = '" + local + "' OR cidade = '" + local +"') "
+								+ "AND	id IN ("
+								+ "		SELECT id_hotel"
+								+ "		FROM 	disponibilidade"
+								+ "		WHERE	disponivel = 1";
+						if (dataInicio.length > 0 && dataFim.length > 0) {
+							sql = sql + "	AND data >= '" + dataInicio + "'"
+									  + "	AND data <= '" + dataFim + "'";
+						}
+						sql = sql + ");";
+
+						connection.query(sql, function (err, rows) {		
+							if (err) throw err;
+
+							// store new data in cache
+							cache.set(cacheKey, rows);
+
+							connection.release();
+						});
+
+					});
+
+				});
+
+			} else {
+				// in cache: send result
+				res.json(result);
+			}
+		});
+
+	});
+
+// ================== suggest route ==========================
+
+app.post('/suggest', function(req, res) {
+
+	var term = req.body["term"];
+
+	const maxSuggestions = 6;
+	var cacheKey = term;
+
+	// check if the key is cached
+	cache.get(cacheKey, function (err, result) {
+		if (err) throw err;
+
+		if (result == undefined) {
+			// not in cache: manual search
+			database.getConnection(function (err, connection) {
+				if (err) throw err;
+
+				connection.query('USE hotelurbano', function (err) {
+					if (err) throw err;
+
+					sql = "SELECT cidade " 
+						+ "FROM	hoteis "
+						+ "WHERE cidade LIKE CONCAT('" + term + "', '%') UNION "
+						+ "SELECT nome " 
+						+ "FROM	hoteis "
+						+ "WHERE nome LIKE CONCAT('" + term + "', '%');";
+
+					console.log(sql);
+					connection.query(sql, function (err, rows) {		
+						if (err) throw err;
+						
+						var data = [];
+						for (var i = 0; i < rows.length && i < maxSuggestions; i++) {
+							data.push(rows[i].cidade);
+						}
+						data = JSON.stringify(data);
+						
+						// store new data in cache
+						cache.set(cacheKey, data);
+
+						res.end(data);
+						connection.release();
+					});
+
+				});
+			});
+
+		} else {
+			// in cache: send result
+			res.end(result);
+		}
+				
+	});
+
+});
 	
 }
